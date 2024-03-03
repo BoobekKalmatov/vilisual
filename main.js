d937cf25682c4906f34f08a71acbc03ab4b5aa0e
awym.dev <awym.dev01@gmail.com>
2024-03-03 22:22:30 +0300

    create animade

---
+++ main.js
@@ -0,0 +1,150 @@
+
+var sliderCounter = 0;
+var sliderContent = [
+  "Web Development",
+  "WordPress Development",
+  "App Development",
+  "Plugin Development",
+  "CRM Integrations"
+];
+var slider = document.querySelector("#slider");
+var sliderValue = document.querySelector("#sliderValue");
+
+function slide() {
+  if (sliderCounter >= sliderContent.length) {
+    sliderCounter = 0;
+  }
+
+  sliderValue.innerHTML = "";
+  
+  sliderValue.classList.remove("holder-animation");
+  void sliderValue.offsetWidth;
+  sliderValue.classList.add("holder-animation");
+  
+  for (i = 0; i < sliderContent[sliderCounter].length; i++) {
+    let letterDiv = document.createElement("div");
+    letterDiv.innerHTML = sliderContent[sliderCounter][i];
+
+    if (letterDiv.innerHTML == " ") {
+      letterDiv.innerHTML = "&nbsp;";
+    }
+    letterDiv.classList.add("start");
+    letterDiv.classList.add("animation");
+    letterDiv.style.animationDelay = i / 10 + "s";
+    sliderValue.appendChild(letterDiv);
+  }
+
+  sliderCounter++;
+}
+
+slide();
+setInterval(slide, 3000);
+
+
+//// js header btn
+
+class ClickSpark extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.root = document.documentElement;
+    this.svg;
+  }
+
+  get activeEls() {
+    return this.getAttribute("active-on");
+  }
+
+  connectedCallback() {
+    this.setupSpark();
+
+    this.root.addEventListener("click", (e) => {
+      if (this.activeEls && !e.target.matches(this.activeEls)) return;
+
+      this.setSparkPosition(e);
+      this.animateSpark();
+    });
+  }
+
+  animateSpark() {
+    let sparks = [...this.svg.children];
+    let size = parseInt(sparks[0].getAttribute("y1"));
+    let offset = size / 2 + "px";
+
+    let keyframes = (i) => {
+      let deg = `calc(${i} * (360deg / ${sparks.length}))`;
+
+      return [
+        {
+          strokeDashoffset: size * 3,
+          transform: `rotate(${deg}) translateY(${offset})`
+        },
+        {
+          strokeDashoffset: size,
+          transform: `rotate(${deg}) translateY(0)`
+        }
+      ];
+    };
+
+    let options = {
+      duration: 660,
+      easing: "cubic-bezier(0.25, 1, 0.5, 1)",
+      fill: "forwards"
+    };
+
+    sparks.forEach((spark, i) => spark.animate(keyframes(i), options));
+  }
+
+  setSparkPosition(e) {
+    let rect = this.root.getBoundingClientRect();
+
+    this.svg.style.left =
+      e.clientX - rect.left - this.svg.clientWidth / 2 + "px";
+    this.svg.style.top =
+      e.clientY - rect.top - this.svg.clientHeight / 2 + "px";
+  }
+
+  setupSpark() {
+    let template = `
+      <style>
+        :host {
+          display: contents;
+        }
+        
+        svg {
+          color: #ffffff;
+          pointer-events: none;
+          position: absolute;
+          rotate: -20deg;
+          stroke: var(--click-spark-color, currentcolor);
+        }
+
+        line {
+          stroke-dasharray: 30;
+          stroke-dashoffset: 30;
+          transform-origin: center;
+        }
+      </style>
+      <svg width="30" height="30" viewBox="0 0 100 100" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="4">
+        ${Array.from(
+          { length: 8 },
+          (_) => `<line x1="50" y1="30" x2="50" y2="4"/>`
+        ).join("")}
+      </svg>
+    `;
+
+    this.shadowRoot.innerHTML = template;
+    this.svg = this.shadowRoot.querySelector("svg");
+  }
+}
+
+customElements.define("click-spark", ClickSpark);
+
+/** Demo scripts **/
+
+const spark = document.querySelector("click-spark");
+const colorPicker = document.getElementById("click-spark-color");
+
+colorPicker.addEventListener("change", () => {
+  spark.style.color('#fff');
+});
